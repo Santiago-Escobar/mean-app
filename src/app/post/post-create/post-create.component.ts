23bf2a71650ada61d534bf539568062af1eeb10a
Santiago-Escobar <davidsantiagoescobarq@gmail.com>
2020-03-17 23:52:52 -0300

    simple form validations with lcoal references

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component , EventEmitter, Output} from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -10,10 +11,13 @@ export class PostCreateComponent  {
   enteredContent = "";
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost(){
+  onAddPost(form : NgForm){
+    if(form.invalid){
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.Content
     };
     this.postCreated.emit(post);
   }
